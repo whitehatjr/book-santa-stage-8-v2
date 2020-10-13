81641a9dced03d95a29f4f84586faa9230d37505
vishalgaddam873 <vishal18@navgurukul.org>
2020-10-13 23:33:05 +0530

    Code Changed

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -5,14 +5,6 @@ import { StyleSheet } from "react-native";
 const MyHeader = props => {
   return (
     <Header
-      leftComponent={
-        <Icon
-          name={"bars"}
-          type={"font-awesome"}
-          color={"#696969"}
-          onPress={() => props.navigation.toggleDrawer()}
-        />
-      }
       centerComponent={{
         text: props.title,
         style: styles.title
